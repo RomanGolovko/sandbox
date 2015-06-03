321949cbf2801add7215cc1476fe2a0deca09695
Roman Golovko <luck-8@yandex.ru>
2015-06-03 15:44:54 +0300

    addition

--- Vehicle_department/Garage.Infrastructure/Repository.cs
+++ Vehicle_department/Garage.Infrastructure/Repository.cs
@@ -1,15 +1,59 @@
 ﻿using System;
 using System.Linq;
+using System.Collections.Generic;
 using System.Windows.Forms;
 using System.Data.Entity;
 using Garage.Domain;
-using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace Garage.Infrastructure
 {
-    public class DAL
+    public class Repository : IDisposable, IRepository
     {
-        public GarageContext db = new GarageContext();
+        private GarageContext db = new GarageContext();
+
+        // drivers enumerator
+        public IEnumerable<Driver> DriversList()
+        {
+            return db.Drivers;
+        }
+        // vehicle enumerator
+        public IEnumerable<Vehicle> VehiclesList()
+        {
+            return db.Vehicles;
+        }
+        // current driver
+        public Driver GetDriver(Guid id)
+        {
+            return db.Drivers.Find(id);
+        }
+        // current vehicle
+        public Vehicle GetVehicles(Guid id)
+        {
+            return db.Vehicles.Find(id);
+        }
+        // load drivers in DB
+        public void LoadDrivers()
+        {
+            db.Drivers.Load();
+
+        }
+        // load vehicles in DB
+        public void LoadVehicles()
+        {
+            db.Vehicles.Load();
+        }
+        // binding DataGdid with drivers
+        public BindingList<Driver>BindDrivers()
+        {
+            return db.Drivers.Local.ToBindingList();
+        }
+        // binding DataGdid with drivers
+        public BindingList<Vehicle> BindVehicles()
+        {
+            return db.Vehicles.Local.ToBindingList();
+        }
+
 
         // remind about technical service
         public void Reminder()
@@ -20,23 +64,9 @@ namespace Garage.Infrastructure
                 temp += serv.Brand + " " + serv.StateNum + " ";
 
             if (temp.Trim() != "")
-                MessageBox.Show("Next Technical Service at the " + temp, "Vehicle Department", 
+                MessageBox.Show("Next Technical Service at the " + temp, "Vehicle Department",
                     MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
-
-        // find current driver
-        public Driver CurrentDriver(Guid id)
-        {
-            Driver driver = db.Drivers.Find(id);
-            return driver;
-        }
-        // find current vehicle
-        public Vehicle CurrentVehicle(Guid id)
-        {
-            Vehicle vehicle = db.Vehicles.Find(id);
-            return vehicle;
-        }
-
         // search drivers
         public List<Driver> DriversSearchedRows(string searchedValue)
         {
@@ -50,19 +80,17 @@ namespace Garage.Infrastructure
         public List<Vehicle> VehiclesSearchedRows(string searchedValue)
         {
             List<Vehicle> searchedRows = new List<Vehicle>();
-            foreach (var row in db.Vehicles.Where(v => v.Brand.Contains(searchedValue)).Union( 
+            foreach (var row in db.Vehicles.Where(v => v.Brand.Contains(searchedValue)).Union(
                                 db.Vehicles.Where(v => v.StateNum.Contains(searchedValue)).Union(
                                 db.Vehicles.Where(v => v.Color.Contains(searchedValue)).Union(
                                 db.Vehicles.Where(v => v.VinCode.Contains(searchedValue))))))
                 searchedRows.Add(row);
 
             return searchedRows;
-
         }
-
         // add / edit driver
-        public bool AddEditDriver(string name, DateTime birthDate, string category,
-            string phoneNum, DateTime medicalCertificate,string id, bool addFlag)
+        public bool AddEditDriver(string name, DateTime birthDate, string category, 
+            string phoneNum, DateTime medicalCertificate, string id, bool addFlag)
         {
             bool flagResult = false;
 
@@ -93,13 +121,12 @@ namespace Garage.Infrastructure
 
             return flagResult;
         }
-
         // deleting current driver
         public bool DelDriver(Guid id)
         {
             bool flagResult = false;
 
-            var driver = CurrentDriver(id);
+            var driver = GetDriver(id);
 
             try
             {
@@ -115,11 +142,9 @@ namespace Garage.Infrastructure
 
             return flagResult;
         }
-
         // add / edit vehicle
-        public bool AddEditVehicle(string brand, string stateNum, string color, 
-            DateTime releaseDate, string vinCode, string mileage, DateTime insurance, 
-            string driverId, string id, bool addFlag)
+        public bool AddEditVehicle(string brand, string stateNum, string color, DateTime releaseDate,
+            string vinCode, string mileage, DateTime insurance, string driverId, string id, bool addFlag)
         {
             bool flagResult = false;
 
@@ -153,13 +178,12 @@ namespace Garage.Infrastructure
 
             return flagResult;
         }
-
         // deleting current vehicle
         public bool DelVehicle(Guid id)
         {
             bool flagResult = false;
 
-            var vehicle = CurrentVehicle(id);
+            var vehicle = GetVehicles(id);
 
             try
             {
@@ -174,6 +198,23 @@ namespace Garage.Infrastructure
             }
 
             return flagResult;
+        }
+
+        protected void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (db != null)
+                {
+                    db.Dispose();
+                    db = null;
+                }
+            }
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
         }
     }
 }
